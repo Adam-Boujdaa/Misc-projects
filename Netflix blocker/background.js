8de5b872d41ceef93895fb9db9f78a5e13a0d095
Adam-Boujdaa <adam.boujdaa@univ-tlse3.fr>
2025-03-17 11:13:41 +0100

    Update background.js

--- Netflix blocker/background.js
+++ Netflix blocker/background.js
@@ -5,8 +5,32 @@
 chrome.webRequest.onBeforeRequest.addListener(
   //The first is the callback function that needs details about the request that is supposed to get sent :
     function(details) {return {cancel : true}},
-  // Filtering argument because otherwise it will block all requests from the browser : Here we want to block netflix requests so 
-    {urls: ["://.netflix.com/" , "https://www.netflix.com/"]}
+  // Filtering argument because otherwise it will block all requests from the browser : Here we want to block these websites only
+    {urls: [
+      // Netflix
+      "*://*.netflix.com/*",
+      "*://*.nflxext.com/*",
+      "*://*.nflximg.com/*",
+      
+      // Amazon Prime Video
+      "*://*.primevideo.com/*",
+      "*://*.amazon.com/gp/video/*",
+      "*://*.media-amazon.com/*",
+      
+      // Crunchyroll
+      "*://*.crunchyroll.com/*",
+      "*://*.crunchyrollbeta.com/*",
+      
+      // Disney+
+      "*://*.disneyplus.com/*",
+      "*://*.disney-plus.com/*",
+      "*://*.dssott.com/*",
+      
+      // Twitch
+      "*://*.twitch.tv/*",
+      "*://*.ttvnw.net/*"
+    ]
+  },
   //Block "function"
     [blocking]
-)    
+);    
